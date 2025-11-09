354a1adacbcdc61e930d9d459e17fe854124d8d8
soulonyoso-design <soulonyoso@gmail.com>
2025-11-09 16:58:47 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let timeleft = 1000;
+const timerElement = document.getElementById("timer");
+
+const countdown = setInterval(() =>{
+timeleft--;
+timerElement.textContent = timeleft;
+
+if (timeleft <= 0){
+  window.location.href="https://soulonyoso-design.github.io/rainbowise.exe/";
+}}, 1000)
